067b09f1c8911cf5ed5c541a7651d07b7cb3980f
Deng Ming <mingflycash@gmail.com>
2022-05-26 22:57:30 +0800

    pool: 泛型 sync.Pool 的封装

---
+++ script/setup.sh
@@ -0,0 +1,29 @@
+# Copyright 2021 gotomicro
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+SOURCE=.github/pre-push
+TARGET=.git/hooks/pre-push
+
+# copy pre-push file if not exist.
+if [ ! -f $TARGET ]; then
+    echo "设置 github hooks..."
+    cp $SOURCE $TARGET
+fi
+
+# add permission to TARGET file.
+test -x $TARGET || chmod +x $TARGET
+
+echo "安装 golangci-lint..."
+# install golangci-lint when golangci-lint not installed.
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.42.1
