a45c4c36d18ff4eeebfb78cdb9feb6295398e1f9
Deng Ming <mingflycash@gmail.com>
2022-08-11 14:21:04 +0800

    定义 List 接口，并且定义 ArrayList 结构体

---
+++ list/array_list.go
@@ -0,0 +1,74 @@
+// Copyright 2021 gotomicro
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package list
+
+// ArrayList 基于切片的简单封装
+type ArrayList[T any] struct {
+	vals []T
+}
+
+func NewArrayList[T any](cap int) *ArrayList[T] {
+	panic("implement me")
+}
+
+// NewArrayListOf 直接使用 ts，而不会执行复制
+func NewArrayListOf[T any](ts []T) *ArrayList[T] {
+	panic("implement me")
+}
+
+func (a *ArrayList[T]) Get(index int) (T, error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (a *ArrayList[T]) Append(t T) error {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (a *ArrayList[T]) Add(index int, t T) error {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (a *ArrayList[T]) Set(index int, t T) error {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (a *ArrayList[T]) Delete(index int) (T, error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (a *ArrayList[T]) Len() int {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (a *ArrayList[T]) Cap() int {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (a *ArrayList[T]) Range(fn func(index int, t T) error) error {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (a *ArrayList[T]) AsSlice() []T {
+	// TODO implement me
+	panic("implement me")
+}
---
+++ list/errors.go
@@ -0,0 +1,22 @@
+// Copyright 2021 gotomicro
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package list
+
+import "fmt"
+
+// newErrIndexOutOfRange 创建一个代表
+func newErrIndexOutOfRange(length int, index int) error {
+	return fmt.Errorf("ekit: 下标超出范围，长度 %d, 下标 %d", length, index)
+}
