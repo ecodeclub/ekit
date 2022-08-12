717942698fa05b0215a2b55653182b3733d101c9
linkai-pro <1134842727@qq.com>
2022-08-12 14:55:42 +0800

    补充ArrayList Add()注释说明

--- list/array_list.go
+++ list/array_list.go
@@ -40,6 +40,9 @@ func (a *ArrayList[T]) Append(t T) error {
 	panic("implement me")
 }
 
+// Add() 在ArrayList下标为index的位置插入一个元素
+// 当index等于ArrayList长度等同于append
+
 func (a *ArrayList[T]) Add(index int, t T) error {
 	if index < 0 || index > len(a.vals) {
 		return newErrIndexOutOfRange(len(a.vals), index)
