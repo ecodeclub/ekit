1713f0eb3657364f640f4c20bd3a928f053ec192
zxl5802316 <1024659300@qq.com>
2022-08-12 20:34:39 +0800

    feat:实现 Arraylist

--- list/array_list.go
+++ list/array_list.go
@@ -20,59 +20,99 @@ type ArrayList[T any] struct {
 }
 
 func NewArrayList[T any](cap int) *ArrayList[T] {
-	panic("implement me")
+	return &ArrayList[T]{
+		vals: make([]T, 0, cap),
+	}
 }
 
 // NewArrayListOf 直接使用 ts，而不会执行复制
 func NewArrayListOf[T any](ts []T) *ArrayList[T] {
-	panic("implement me")
+	return &ArrayList[T]{
+		vals: ts,
+	}
 }
 
+// Get 返回对应下标的元素，
+// 在下标超出范围的情况下，返回错误
 func (a *ArrayList[T]) Get(index int) (T, error) {
-	// TODO implement me
-	panic("implement me")
+	if a.Len() <= index || index < 0 {
+		// 笔记： T 不是一个确定的类型，最好不要返回 nil ，可以给一个默认的 T 的类型的初始值
+		//例如： var i int = nil
+		var defaultT T
+		return defaultT, newErrIndexOutOfRange(a.Len(), index)
+	}
+	return a.vals[index], nil
 }
 
+// Append 在末尾追加元素
 func (a *ArrayList[T]) Append(t T) error {
-	// TODO implement me
-	panic("implement me")
+	a.vals = append(a.vals, t)
+	return nil
 }
 
+// Add 在特定下标处增加一个新元素
+// 如果下标超出范围，应该返回错误
 func (a *ArrayList[T]) Add(index int, t T) error {
-	// TODO implement me
-	panic("implement me")
+	if a.Len() < index || index < 0 {
+		return newErrIndexOutOfRange(a.Len(), index)
+	}
+	// 如果 index == slice length ; 说明这是末尾添加一个 t
+	if len(a.vals) == index {
+		return a.Append(t)
+	}
+	source := append([]T{}, a.vals...)
+	a.vals = append(a.vals[:index], t)
+	a.vals = append(a.vals, source[index:]...)
+	return nil
 }
 
+// Set 重置 index 位置的值
+// 如果下标超出范围，应该返回错误
 func (a *ArrayList[T]) Set(index int, t T) error {
-	// TODO implement me
-	panic("implement me")
+	if a.Len() <= index || index < 0 {
+		return newErrIndexOutOfRange(a.Len(), index)
+	}
+	a.vals[index] = t
+	return nil
 }
 
+// Delete 删除目标元素的位置，并且返回该位置的值
+// 如果 index 超出下标，应该返回错误
 func (a *ArrayList[T]) Delete(index int) (T, error) {
-	// TODO implement me
-	panic("implement me")
+	if a.Len() <= index || index < 0 {
+		var defaultT T
+		return defaultT, newErrIndexOutOfRange(a.Len(), index)
+	}
+	t := a.vals[index]
+	a.vals = append(a.vals[:index], a.vals[index+1:]...)
+	return t, nil
 }
 
+// Len 返回长度
 func (a *ArrayList[T]) Len() int {
-	// TODO implement me
-	panic("implement me")
+	return len(a.vals)
 }
 
+// Cap 返回容量
 func (a *ArrayList[T]) Cap() int {
 	return cap(a.vals)
 }
 
+// Range 遍历 List 的所有元素
 func (a *ArrayList[T]) Range(fn func(index int, t T) error) error {
-	for key, value := range a.vals {
-		e := fn(key, value)
-		if e != nil {
-			return e
+	for i, t := range a.vals {
+		err := fn(i, t)
+		if err != nil {
+			return err
 		}
 	}
 	return nil
 }
 
+// AsSlice 将 List 转化为一个切片
+// 不允许返回nil，在没有元素的情况下，
+// 必须返回一个长度和容量都为 0 的切片
+// AsSlice 每次调用都必须返回一个全新的切片
 func (a *ArrayList[T]) AsSlice() []T {
-	// TODO implement me
-	panic("implement me")
+	return append([]T{}, a.vals...)
 }
--- list/array_list_test.go
+++ list/array_list_test.go
@@ -17,48 +17,117 @@ package list
 import (
 	"errors"
 	"fmt"
-	"testing"
-
 	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
-//	func TestArrayList_Add(t *testing.T) {
-//		testCases := []struct {
-//			name      string
-//			list      *ArrayList[int]
-//			index     int
-//			newVal    int
-//			wantSlice []int
-//			wantErr   error
-//		}{
-//			// 仿照这个例子，继续添加测试
-//			// 你需要综合考虑下标的各种可能取值
-//			// 往两边增加，往中间加
-//			// 下标可能是负数，也可能超出你的长度
-//			{
-//				name:      "index 0",
-//				list:      NewArrayListOf[int]([]int{123}),
-//				newVal:    100,
-//				wantSlice: []int{100, 123},
-//			},
-//		}
-//
-//		for _, tc := range testCases {
-//			t.Run(tc.name, func(t *testing.T) {
-//				err := tc.list.Add(tc.index, tc.newVal)
-//				assert.Equal(t, tc.wantErr, err)
-//				// 因为返回了 error，所以我们不用继续往下比较了
-//				if err != nil {
-//					return
-//				}
-//				assert.Equal(t, tc.wantSlice, tc.list.vals)
-//			})
-//		}
-//	}
-//
-//	func TestArrayList_Append(t *testing.T) {
-//		// 这个比较简单，只需要增加元素，然后判断一下 Append 之后是否符合预期
-//	}
+func TestArrayList_Add(t *testing.T) {
+	testCases := []struct {
+		name      string
+		list      *ArrayList[int]
+		index     int
+		newVal    int
+		wantSlice []int
+		wantErr   error
+	}{
+		// 仿照这个例子，继续添加测试
+		// 你需要综合考虑下标的各种可能取值
+		// 往两边增加，往中间加
+		// 下标可能是负数，也可能超出你的长度
+		{
+			name:      "index 0",
+			list:      NewArrayListOf[int]([]int{123}),
+			newVal:    100,
+			wantSlice: []int{100, 123},
+		},
+		{
+			name:    "index -1",
+			list:    NewArrayListOf[int]([]int{123}),
+			index:   -1,
+			newVal:  12,
+			wantErr: newErrIndexOutOfRange(1, -1),
+		},
+		{
+			name:    "index 10",
+			list:    NewArrayListOf[int]([]int{123}),
+			index:   10,
+			newVal:  12,
+			wantErr: newErrIndexOutOfRange(1, 10),
+		},
+		{
+			name:      "index 1",
+			list:      NewArrayListOf[int]([]int{123}),
+			index:     1,
+			newVal:    12,
+			wantSlice: []int{123, 12},
+		},
+		{
+			name:      "index 2",
+			list:      NewArrayListOf[int]([]int{1, 2, 3, 4, 5, 6}),
+			index:     2,
+			newVal:    12,
+			wantSlice: []int{1, 2, 12, 3, 4, 5, 6},
+		},
+		{
+			name:      "index == len",
+			list:      NewArrayListOf[int]([]int{1, 2, 3, 4, 5, 6}),
+			index:     6,
+			newVal:    12,
+			wantSlice: []int{1, 2, 3, 4, 5, 6, 12},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := tc.list.Add(tc.index, tc.newVal)
+			assert.Equal(t, tc.wantErr, err)
+			// 因为返回了 error，所以我们不用继续往下比较了
+			if err != nil {
+				return
+			}
+			assert.Equal(t, tc.wantSlice, tc.list.vals)
+		})
+	}
+}
+
+func TestArrayList_Append(t *testing.T) {
+	// 这个比较简单，只需要增加元素，然后判断一下 Append 之后是否符合预期
+	testCases := []struct {
+		name      string
+		list      *ArrayList[int]
+		newVal    int
+		wantSlice []int
+		wantErr   error
+	}{
+		{
+			name:      "append 2",
+			list:      NewArrayListOf[int]([]int{1, 2, 3, 4}),
+			newVal:    2,
+			wantSlice: []int{1, 2, 3, 4, 2},
+			wantErr:   nil,
+		},
+		{
+			name:      "append 3",
+			list:      NewArrayListOf[int]([]int{1, 2, 3, 4}),
+			newVal:    3,
+			wantSlice: []int{1, 2, 3, 4, 3},
+			wantErr:   nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := tc.list.Append(tc.newVal)
+			//这个可以省略 没有err 都是nil start
+			assert.Equal(t, tc.wantErr, err)
+			if err != nil {
+				return
+			}
+			//这个可以省略 没有err 都是nil end
+			assert.Equal(t, tc.wantSlice, tc.list.vals)
+		})
+	}
+}
 
 func TestArrayList_Cap(t *testing.T) {
 	testCases := []struct {
@@ -107,83 +176,62 @@ func BenchmarkArrayList_Cap(b *testing.B) {
 	})
 }
 
-//	func TestArrayList_Append(t *testing.T) {
-//		// 这个比较简单，只需要增加元素，然后判断一下 Append 之后是否符合预期
-//	}
+func TestArrayList_Delete(t *testing.T) {
+	testCases := []struct {
+		name      string
+		list      *ArrayList[int]
+		index     int
+		wantSlice []int
+		wantVal   int
+		wantErr   error
+	}{
+		// 仿照这个例子，继续添加测试
+		// 你需要综合考虑下标的各种可能取值
+		// 往两边增加，往中间加
+		// 下标可能是负数，也可能超出你的长度
+		{
+			name:      "index 0",
+			list:      NewArrayListOf[int]([]int{123, 100}),
+			index:     0,
+			wantSlice: []int{100},
+			wantVal:   123,
+		},
+		{
+			name:    "index -1",
+			list:    NewArrayListOf[int]([]int{123, 100}),
+			index:   -1,
+			wantErr: newErrIndexOutOfRange(2, -1),
+		},
+		{
+			name:    "index 10",
+			list:    NewArrayListOf[int]([]int{123, 100}),
+			index:   10,
+			wantErr: newErrIndexOutOfRange(2, 10),
+		},
+		{
+			name:      "index 1",
+			list:      NewArrayListOf[int]([]int{123, 100, 150}),
+			index:     1,
+			wantSlice: []int{123, 150},
+			wantVal:   100,
+		},
+	}
 
-//	func TestArrayList_Delete(t *testing.T) {
-//		testCases := []struct {
-//			name      string
-//			list      *ArrayList[int]
-//			index     int
-//			wantSlice []int
-//			wantVal   int
-//			wantErr   error
-//		}{
-//			// 仿照这个例子，继续添加测试
-//			// 你需要综合考虑下标的各种可能取值
-//			// 往两边增加，往中间加
-//			// 下标可能是负数，也可能超出你的长度
-//			{
-//				name:      "index 0",
-//				list:      NewArrayListOf[int]([]int{123, 100}),
-//				index:     0,
-//				wantSlice: []int{100},
-//				wantVal:   123,
-//			},
-//		}
-//
-//		for _, tc := range testCases {
-//			t.Run(tc.name, func(t *testing.T) {
-//				val, err := tc.list.Delete(tc.index)
-//				assert.Equal(t, tc.wantErr, err)
-//				// 因为返回了 error，所以我们不用继续往下比较了
-//				if err != nil {
-//					return
-//				}
-//				assert.Equal(t, tc.wantSlice, tc.list.vals)
-//				assert.Equal(t, tc.wantVal, val)
-//			})
-//		}
-//	}
-//
-//	func TestArrayList_Get(t *testing.T) {
-//		testCases := []struct {
-//			name    string
-//			list    *ArrayList[int]
-//			index   int
-//			wantVal int
-//			wantErr error
-//		}{
-//			// 仿照这个例子，继续添加测试
-//			// 你需要综合考虑下标的各种可能取值
-//			// 往两边增加，往中间加
-//			// 下标可能是负数，也可能超出你的长度
-//			{
-//				name:    "index 0",
-//				list:    NewArrayListOf[int]([]int{123, 100}),
-//				index:   0,
-//				wantVal: 123,
-//			},
-//		}
-//
-//		for _, tc := range testCases {
-//			t.Run(tc.name, func(t *testing.T) {
-//				val, err := tc.list.Get(tc.index)
-//				assert.Equal(t, tc.wantErr, err)
-//				// 因为返回了 error，所以我们不用继续往下比较了
-//				if err != nil {
-//					return
-//				}
-//				assert.Equal(t, tc.wantVal, val)
-//			})
-//		}
-//	}
-func TestArrayList_Range(t *testing.T) {
-	// 设计两个测试用例，用求和来作为场景
-	// 一个测试用例是计算全部元素的和
-	// 一个测试用例是计算元素的和，如果遇到了第一个负数，那么就中断返回
-	// 测试最终断言求的和是否符合预期
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			val, err := tc.list.Delete(tc.index)
+			assert.Equal(t, tc.wantErr, err)
+			// 因为返回了 error，所以我们不用继续往下比较了
+			if err != nil {
+				return
+			}
+			assert.Equal(t, tc.wantSlice, tc.list.vals)
+			assert.Equal(t, tc.wantVal, val)
+		})
+	}
+}
+
+func TestArrayList_Get(t *testing.T) {
 	testCases := []struct {
 		name    string
 		list    *ArrayList[int]
@@ -191,24 +239,68 @@ func TestArrayList_Range(t *testing.T) {
 		wantVal int
 		wantErr error
 	}{
+		// 仿照这个例子，继续添加测试
+		// 你需要综合考虑下标的各种可能取值
+		// 往两边增加，往中间加
+		// 下标可能是负数，也可能超出你的长度
 		{
-			name: "计算全部元素的和",
-			list: &ArrayList[int]{
-				vals: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
-			},
-			wantVal: 55,
-			wantErr: nil,
+			name:    "index 0",
+			list:    NewArrayListOf[int]([]int{123, 100}),
+			index:   0,
+			wantVal: 123,
 		},
 		{
-			name: "测试中断",
-			list: &ArrayList[int]{
-				vals: []int{1, 2, 3, 4, -5, 6, 7, 8, -9, 10},
-			},
-			wantVal: 41,
-			wantErr: errors.New("index 4 is error"),
+			name:    "index 2",
+			list:    NewArrayListOf[int]([]int{123, 100}),
+			index:   2,
+			wantErr: newErrIndexOutOfRange(2, 2),
 		},
 		{
-			name: "测试数组为nil",
+			name:    "index -1",
+			list:    NewArrayListOf[int]([]int{123, 100}),
+			index:   -1,
+			wantErr: newErrIndexOutOfRange(2, -1),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			val, err := tc.list.Get(tc.index)
+			assert.Equal(t, tc.wantErr, err)
+			// 因为返回了 error，所以我们不用继续往下比较了
+			if err != nil {
+				return
+			}
+			assert.Equal(t, tc.wantVal, val)
+		})
+	}
+}
+
+//
+func TestArrayList_Range(t *testing.T) {
+	// 设计两个测试用例，用求和来作为场景
+	// 一个测试用例是计算全部元素的和
+	// 一个测试用例是计算元素的和，如果遇到了第一个负数，那么就中断返回
+	// 测试最终断言求的和是否符合预期
+	pluralErr := errors.New("value cannot be complex")
+	testCases := []struct {
+		name    string
+		list    *ArrayList[int]
+		wantVal int
+		wantErr error
+	}{
+		{
+			name:    "Range sum",
+			list:    NewArrayListOf[int]([]int{1, 2, 3}),
+			wantVal: 6,
+		},
+		{
+			name:    "Range -1",
+			list:    NewArrayListOf[int]([]int{-1, 2, 3}),
+			wantErr: pluralErr,
+		},
+		{
+			name: "Range nil",
 			list: &ArrayList[int]{
 				vals: nil,
 			},
@@ -218,50 +310,181 @@ func TestArrayList_Range(t *testing.T) {
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			result := 0
-			err := tc.list.Range(func(index int, num int) error {
-				if num < 0 {
-					return fmt.Errorf("index %d is error", index)
+			var sum int
+			err := tc.list.Range(func(index int, t int) error {
+				if t < 0 {
+					return pluralErr
 				}
-				result += num
+				sum += t
 				return nil
 			})
-
 			assert.Equal(t, tc.wantErr, err)
 			if err != nil {
 				return
 			}
-			assert.Equal(t, tc.wantVal, result)
+			assert.Equal(t, tc.wantVal, sum)
+		})
+	}
+}
+
+func TestArrayList_Len(t *testing.T) {
+	testCases := []struct {
+		name string
+		len  int
+		list *ArrayList[int]
+	}{
+		{
+			name: "len 1",
+			len:  1,
+			list: &ArrayList[int]{
+				vals: make([]int, 1),
+			},
+		},
+		{
+			name: "len 0",
+			len:  0,
+			list: &ArrayList[int]{
+				vals: make([]int, 0),
+			},
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			actual := testCase.list.Len()
+			assert.Equal(t, testCase.len, actual)
 		})
 	}
 }
 
 //
-// func TestArrayList_Len(t *testing.T) {
+func TestArrayList_Set(t *testing.T) {
+	testCases := []struct {
+		name      string
+		list      *ArrayList[int]
+		index     int
+		wantSlice []int
+		newVal    int
+		wantErr   error
+	}{
+		{
+			name:      "index 0",
+			list:      NewArrayListOf[int]([]int{123, 100}),
+			index:     0,
+			wantSlice: []int{1, 100},
+			newVal:    1,
+		},
+		{
+			name:    "index -1",
+			list:    NewArrayListOf[int]([]int{123, 100}),
+			index:   -1,
+			wantErr: newErrIndexOutOfRange(2, -1),
+		},
+		{
+			name:    "index 10",
+			list:    NewArrayListOf[int]([]int{123, 100}),
+			index:   10,
+			wantErr: newErrIndexOutOfRange(2, 10),
+		},
+		{
+			name:      "index 1",
+			list:      NewArrayListOf[int]([]int{123, 100, 150}),
+			index:     1,
+			wantSlice: []int{123, 3, 150},
+			newVal:    3,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := tc.list.Set(tc.index, tc.newVal)
+			assert.Equal(t, tc.wantErr, err)
+			// 因为返回了 error，所以我们不用继续往下比较了
+			if err != nil {
+				return
+			}
+			assert.Equal(t, tc.wantSlice, tc.list.vals)
+		})
+	}
+}
+
 //
-// }
-//
-// func TestArrayList_Set(t *testing.T) {
-//
-// }
-//
-// func TestArrayList_AsSlice(t *testing.T) {
-// 	vals := []int{1, 2, 3}
-// 	a := NewArrayListOf[int](vals)
-// 	slice := a.AsSlice()
-// 	// 内容相同
-// 	assert.Equal(t, slice, vals)
-// 	aAddr := fmt.Sprintf("%p", vals)
-// 	sliceAddr := fmt.Sprintf("%p", slice)
-// 	// 但是地址不同，也就是意味着 slice 必须是一个新创建的
-// 	assert.NotEqual(t, aAddr, sliceAddr)
-// }
-//
-// // 为其它所有的公开方法都加上例子
-// func ExampleArrayList_Add() {
-// 	list := NewArrayListOf[int]([]int{1, 2, 3})
-// 	_ = list.Add(0, 9)
-// 	fmt.Println(list.AsSlice())
-// 	// Output:
-// 	// [9 1 2 3]
-// }
+func TestArrayList_AsSlice(t *testing.T) {
+	vals := []int{1, 2, 3}
+	a := NewArrayListOf[int](vals)
+	slice := a.AsSlice()
+	// 内容相同
+	assert.Equal(t, slice, vals)
+	aAddr := fmt.Sprintf("%p", vals)
+	sliceAddr := fmt.Sprintf("%p", slice)
+	// 但是地址不同，也就是意味着 slice 必须是一个新创建的
+	assert.NotEqual(t, aAddr, sliceAddr)
+}
+
+// 为其它所有的公开方法都加上例子
+func ExampleArrayList_Add() {
+	list := NewArrayListOf[int]([]int{1, 2, 3})
+	_ = list.Add(0, 9)
+	fmt.Println(list.AsSlice())
+	// Output:
+	// [9 1 2 3]
+}
+
+func ExampleArrayList_Append() {
+	list := NewArrayListOf[int]([]int{1, 2, 3})
+	_ = list.Append(9)
+	fmt.Println(list.AsSlice())
+	// Output:
+	// [1 2 3 9]
+}
+
+func ExampleArrayList_Get() {
+	list := NewArrayListOf[int]([]int{1, 2, 3})
+	val, _ := list.Get(1)
+	fmt.Println(val)
+	// Output:
+	// 2
+}
+
+func ExampleArrayList_Delete() {
+	list := NewArrayListOf[int]([]int{1, 2, 3})
+	val, _ := list.Delete(1)
+	fmt.Println(val)
+	// Output:
+	// 2
+}
+
+func ExampleArrayList_Set() {
+	list := NewArrayListOf[int]([]int{1, 2, 3})
+	_ = list.Set(1, 3)
+	fmt.Println(list.AsSlice())
+	// Output:
+	// [1 3 3]
+}
+
+func ExampleArrayList_Len() {
+	list := NewArrayListOf[int]([]int{1, 2, 3})
+	n := list.Len()
+	fmt.Println(n)
+	// Output:
+	// 3
+}
+
+func ExampleArrayList_Cap() {
+	list := NewArrayListOf[int]([]int{1, 2, 3})
+	n := list.Cap()
+	fmt.Println(n)
+	// Output:
+	// 3
+}
+
+func ExampleArrayList_Range() {
+	list := NewArrayListOf[int]([]int{1, 2, 3})
+	sum := 0
+	_ = list.Range(func(i int, val int) error {
+		sum += val
+		return nil
+	})
+	fmt.Println(sum)
+	// Output:
+	// 6
+}
