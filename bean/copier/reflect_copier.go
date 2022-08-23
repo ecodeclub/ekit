6a0c48921b1f411c82e1585531ec40679e7a806b
eric.xie <hhh111119@outlook.com>
2022-08-24 02:54:36 +0800

    改动 reflect copier - 简化了代码 - 规范了命名

--- bean/copier/reflect_copier.go
+++ bean/copier/reflect_copier.go
@@ -34,11 +34,7 @@ func NewReflectCopier[Src any, Dst any]() *ReflectCopier[Src, Dst] {
 // 2. 如果 Src 和 Dst 中匹配的字段，其类型是基本类型（及其指针）或者内置类型（及其指针），并且类型一样，则直接用 Src 的值
 // 3. 如果 Src 和 Dst 中匹配的字段，其类型都是结构体，或者都是结构体指针，那么会深入复制
 // 4. 否则，返回类型不匹配的错误
-// TODO: 支持不同类型之间的转换
 func (r *ReflectCopier[Src, Dst]) CopyTo(src *Src, dst *Dst) error {
-	return r.copyStructPointer(src, dst)
-}
-func (r *ReflectCopier[Src, Dst]) copyStructPointer(src any, dst any) error {
 	srcTyp := reflect.TypeOf(src).Elem()
 	if srcTyp.Kind() != reflect.Struct {
 		return newErrTypeError(srcTyp.Kind())
@@ -51,14 +47,18 @@ func (r *ReflectCopier[Src, Dst]) copyStructPointer(src any, dst any) error {
 	srcValue := reflect.ValueOf(src).Elem()
 	dstValue := reflect.ValueOf(dst).Elem()
 
-	srcFiledNameID := make(map[string]int, 0)
+	return r.copyStruct(srcTyp, srcValue, dstTyp, dstValue)
+}
+
+func (r *ReflectCopier[Src, Dst]) copyStruct(srcTyp reflect.Type, srcValue reflect.Value, dstTyp reflect.Type, dstValue reflect.Value) error {
+	srcFieldNameID := make(map[string]int, 0)
 	//dstFiledNameID := make(map[string]int32, 0)
 	for i := 0; i < srcTyp.NumField(); i += 1 {
 		fTyp := srcTyp.Field(i)
 		if !fTyp.IsExported() {
 			continue
 		}
-		srcFiledNameID[fTyp.Name] = i
+		srcFieldNameID[fTyp.Name] = i
 	}
 
 	for i := 0; i < dstTyp.NumField(); i += 1 {
@@ -66,13 +66,72 @@ func (r *ReflectCopier[Src, Dst]) copyStructPointer(src any, dst any) error {
 		if !fTyp.IsExported() {
 			continue
 		}
-		if idx, ok := srcFiledNameID[fTyp.Name]; ok {
-			if err := r.copyToWithFiledIndex(srcTyp, srcValue, dstTyp, dstValue, idx, i); err != nil {
+		if idx, ok := srcFieldNameID[fTyp.Name]; ok {
+			if err := r.copyStructField(srcTyp, srcValue, dstTyp, dstValue, idx, i); err != nil {
 				return err
 			}
 		}
 	}
 	return nil
+}
+
+func (r *ReflectCopier[Src, Dst]) copyStructField(
+	srcTyp reflect.Type,
+	srcValue reflect.Value,
+	dstTyp reflect.Type,
+	dstValue reflect.Value,
+	srcFiledIndex int,
+	dstFiledIndex int) error {
+
+	srcFieldType := srcTyp.Field(srcFiledIndex)
+	dstFieldType := dstTyp.Field(dstFiledIndex)
+	if srcFieldType.Type.Kind() != dstFieldType.Type.Kind() {
+		return newErrKindNotMatchError(srcFieldType.Type.Kind(), dstFieldType.Type.Kind(), srcFieldType.Name)
+	}
+	srcFiledValue := srcValue.Field(srcFiledIndex)
+	dstFiledValue := dstValue.Field(dstFiledIndex)
+
+	if srcFieldType.Type.Kind() == reflect.Pointer {
+		if srcFiledValue.IsNil() {
+			return nil
+		}
+		if dstFiledValue.IsNil() {
+			dstFiledValue.Set(reflect.New(dstFieldType.Type.Elem()))
+		}
+		return r.copyData(srcFieldType.Type.Elem(), srcFiledValue.Elem(), dstFieldType.Type.Elem(), dstFiledValue.Elem(), srcFieldType.Name)
+	}
+
+	return r.copyData(srcFieldType.Type, srcFiledValue, dstFieldType.Type, dstFiledValue, srcFieldType.Name)
+}
+
+func (r *ReflectCopier[Src, Dst]) copyData(
+	srcTyp reflect.Type,
+	srcValue reflect.Value,
+	dstTyp reflect.Type,
+	dstValue reflect.Value,
+	fieldName string,
+) error {
+	if srcTyp.Kind() == reflect.Pointer {
+		return newErrMultiPointer(fieldName)
+	}
+	if srcTyp.Kind() != dstTyp.Kind() {
+		return newErrKindNotMatchError(srcTyp.Kind(), dstTyp.Kind(), fieldName)
+	}
+
+	if isShadowCopyType(srcTyp.Kind()) {
+		if dstValue.CanSet() {
+			dstValue.Set(srcValue)
+		}
+	} else if srcTyp.Kind() == reflect.Struct {
+		return r.copyStruct(srcTyp, srcValue, dstTyp, dstValue)
+	}
+	return nil
+}
+
+func (r *ReflectCopier[Src, Dst]) Copy(src *Src) (*Dst, error) {
+	dst := new(Dst)
+	err := r.CopyTo(src, dst)
+	return dst, err
 }
 
 func isShadowCopyType(kind reflect.Kind) bool {
@@ -101,63 +160,4 @@ func isShadowCopyType(kind reflect.Kind) bool {
 		return true
 	}
 	return false
-}
-
-func (r *ReflectCopier[Src, Dst]) copyToWithFiledIndex(
-	srcTyp reflect.Type,
-	srcValue reflect.Value,
-	dstTyp reflect.Type,
-	dstValue reflect.Value,
-	srcFiledIndex int,
-	dstFiledIndex int) error {
-
-	srcFiledType := srcTyp.Field(srcFiledIndex)
-	dstFiledType := dstTyp.Field(dstFiledIndex)
-	if srcFiledType.Type.Kind() != dstFiledType.Type.Kind() {
-		return newErrKindNotMatchError(srcFiledType.Type.Kind(), dstFiledType.Type.Kind(), srcFiledType.Name)
-	}
-	srcFiledValue := srcValue.Field(srcFiledIndex)
-	dstFiledValue := dstValue.Field(dstFiledIndex)
-
-	if srcFiledType.Type.Kind() == reflect.Pointer {
-		if srcFiledValue.IsNil() {
-			return nil
-		}
-		if dstFiledValue.IsNil() {
-			dstFiledValue.Set(reflect.New(dstFiledType.Type.Elem()))
-		}
-		return r.copyField(srcFiledType.Type.Elem(), srcFiledValue.Elem(), dstFiledType.Type.Elem(), dstFiledValue.Elem(), srcFiledType.Name)
-	}
-
-	return r.copyField(srcFiledType.Type, srcFiledValue, dstFiledType.Type, dstFiledValue, srcFiledType.Name)
-}
-
-func (r *ReflectCopier[Src, Dst]) copyField(
-	srcTyp reflect.Type,
-	srcValue reflect.Value,
-	dstTyp reflect.Type,
-	dstValue reflect.Value,
-	fieldName string,
-) error {
-	if srcTyp.Kind() == reflect.Pointer {
-		return newErrMultiPointer(fieldName)
-	}
-	if srcTyp.Kind() != dstTyp.Kind() {
-		return newErrKindNotMatchError(srcTyp.Kind(), dstTyp.Kind(), fieldName)
-	}
-
-	if isShadowCopyType(srcTyp.Kind()) {
-		if dstValue.CanSet() {
-			dstValue.Set(srcValue)
-		}
-	} else if srcTyp.Kind() == reflect.Struct {
-		return r.copyStructPointer(srcValue.Addr().Interface(), dstValue.Addr().Interface())
-	}
-	return nil
-}
-
-func (r *ReflectCopier[Src, Dst]) Copy(src *Src) (*Dst, error) {
-	dst := new(Dst)
-	err := r.CopyTo(src, dst)
-	return dst, err
 }
