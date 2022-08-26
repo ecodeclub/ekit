b33086a89f2d4c218df943648b155eec42bd424d
TopWay2Hogwarts <88173543+TopWay2Hogwarts@users.noreply.github.com>
2022-08-26 17:23:12 +0800

    非组合

--- bean/copier/reflect_copier.go
+++ bean/copier/reflect_copier.go
@@ -14,17 +14,63 @@
 
 package copier
 
+import (
+	"errors"
+	"reflect"
+	"unsafe"
+)
+
+// 实际上不需要，但可以记在心里
+// const (
+//
+//	bInvalid   = 1 << 0
+//	bBool      = 1 << 1
+//	bInt       = 1 << 2
+//	bInt8      = 1 << 3
+//	bInt16     = 1 << 4
+//	bInt32     = 1 << 5
+//	bInt64     = 1 << 6
+//	bUint      = 1 << 7
+//	bUint8     = 1 << 8
+//	bUint16    = 1 << 9
+//	bUint32    = 1 << 10
+//	bUint64    = 1 << 11
+//	bUintptr   = 1 << 12
+//	bFloat32   = 1 << 13
+//	bFloat64   = 1 << 14
+//	bComplex64  = 1 << 15
+//	bComplex128  = 1 << 16
+//	bArray     = 1 << 17
+//	bChan      = 1 << 18
+//	bFunc      = 1 << 19
+//	bInterface = 1 << 20
+//	bMap       = 1 << 21
+//	bPointer   = 1 << 22
+//	bSlice     = 1 << 23
+//	bString    = 1 << 24
+//	bStruct    = 1 << 25
+//	bUnsafePointer  = 1 << 26
+//
+// )
+var errInvalidType = errors.New("only support struct")
+
+// |除Invalid外的基本数据类型| Func(一种特殊的指针，应当直接复制) | string | map | slice
+const basicKind = (1<<17 - 1) | (1 << 19) | (1 << 24) | (1 << 21) | (1 << 23)
+
 // ReflectCopier 基于反射的实现
 // ReflectCopier 是浅拷贝
 type ReflectCopier[Src any, Dst any] struct {
 	// fieldMap Src 中字段下标到 Dst 字段下标的映射
 	// 其中 key 是 Src 中字段下标使用连字符 - 连接起来
 	// 不在 fieldMap 中字段则意味着被忽略
-	fieldMap map[string][]int
+	fieldMap map[string]int
 }
 
 func NewReflectCopier[Src any, Dst any]() *ReflectCopier[Src, Dst] {
-	panic("implement me")
+	//panic("implement me")
+	return &ReflectCopier[Src, Dst]{
+		make(map[string]int),
+	}
 }
 
 // CopyTo 执行复制
@@ -33,9 +79,74 @@ func NewReflectCopier[Src any, Dst any]() *ReflectCopier[Src, Dst] {
 // 2. 如果 Src 和 Dst 中匹配的字段，其类型是基本类型（及其指针）或者内置类型（及其指针），并且类型一样，则直接用 Src 的值
 // 3. 如果 Src 和 Dst 中匹配的字段，其类型都是结构体，或者都是结构体指针，那么会深入复制
 // 4. 否则，返回类型不匹配的错误
-// TODO: 支持不同类型之间的转换
+//
+// 需求疑惑的点,关于匹配的粒度问题, 目前仅对完全匹配（名称-类型）的保证深度复制
+// 对于结构体内部的完全匹配，暂时不予考虑，详细见测试用例 Complicated Sub Struct
 func (r *ReflectCopier[Src, Dst]) CopyTo(src *Src, dst *Dst) error {
-	panic("implement me")
+	srcVal := reflect.ValueOf(src).Elem()
+	dstVal := reflect.ValueOf(dst).Elem()
+	//只支持src和dst同时为结构体指针
+	if srcVal.Kind() != reflect.Struct || dstVal.Kind() != reflect.Struct {
+		return errInvalidType
+	}
+	//确定目标字段中的key，并记录其偏移量，以供后面直接操作内存
+	for i := 0; i < dstVal.NumField(); i++ {
+		fileKey := dstVal.Type().Field(i).Name + "-" + dstVal.Field(i).Kind().String()
+		//r.fieldMap[fileKey] = append(r.fieldMap[fileKey], i)
+		r.fieldMap[fileKey] = i
+	}
+
+	for i := 0; i < srcVal.NumField(); i++ {
+		fileKey := srcVal.Type().Field(i).Name + "-" + srcVal.Field(i).Kind().String()
+		if dstFiled, ok := r.fieldMap[fileKey]; ok {
+			srcSettable := structOffsetValue(srcVal, i)
+			dstSettable := structOffsetValue(dstVal, dstFiled)
+			copyTo(srcSettable, dstSettable)
+		}
+	}
+	return nil
+}
+
+func structOffsetValue(value reflect.Value, index int) reflect.Value {
+	return reflect.NewAt(value.Field(index).Type(), unsafe.Pointer(uintptr(value.UnsafeAddr())+value.Type().Field(index).Offset)).Elem()
+}
+
+func copyTo(src reflect.Value, dst reflect.Value) {
+	//如果可以Set，就直接赋值
+	switch src.Kind() {
+	case reflect.Pointer, reflect.UnsafePointer:
+		if src.UnsafePointer() == nil {
+			dst.Set(src)
+		} else {
+			newPtrV := reflect.New(dst.Type().Elem())
+			dst.Set(newPtrV)
+			copyTo(src.Elem(), dst.Elem())
+		}
+		break
+	default:
+		if dst.CanSet() {
+			dst.Set(src)
+			// 对于struct字段中的指针类型额外的处理
+			// 目前只支持一级指针的深度拷贝
+			if dst.Kind() == reflect.Struct {
+				numFiled := dst.NumField()
+				for i := 0; i < numFiled; i++ {
+					value := dst.Field(i)
+					if value.Kind() == reflect.Pointer || value.Kind() == reflect.UnsafePointer {
+						//新建一个类型，并赋值到value上
+						dstNewPtr := reflect.New(value.Type().Elem())
+						value.Set(dstNewPtr)
+						copyTo(dst.Field(i).Elem(), value.Elem())
+					}
+				}
+			}
+		}
+	}
+
+}
+
+func isBasicKind(kind reflect.Kind) bool {
+	return 1<<kind&basicKind > 0
 }
 
 func (r *ReflectCopier[Src, Dst]) Copy(src *Src) (*Dst, error) {
--- bean/copier/reflect_copier_test.go
+++ bean/copier/reflect_copier_test.go
@@ -15,8 +15,10 @@
 package copier
 
 import (
+	"github.com/gotomicro/ekit"
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"unsafe"
 )
 
 func TestReflectCopier_Copy(t *testing.T) {
@@ -26,22 +28,124 @@ func TestReflectCopier_Copy(t *testing.T) {
 		wantDst  any
 		wantErr  error
 	}{
-		// {
-		// 	name: "simple struct",
-		// 	copyFunc: func() (any, error) {
-		// 		copier := NewReflectCopier[SimpleSrc, SimpleDst]()
-		// 		return copier.Copy(&SimpleSrc{
-		// 			Name:    "大明",
-		// 			Age:     ekit.ToPtr[int](18),
-		// 			Friends: []string{"Tom", "Jerry"},
-		// 		})
-		// 	},
-		// 	wantDst: SimpleDst{
-		// 		Name:    "大明",
-		// 		Age:     ekit.ToPtr[int](18),
-		// 		Friends: []string{"Tom", "Jerry"},
-		// 	},
-		// },
+		{
+			name: "error input",
+			copyFunc: func() (any, error) {
+				copier := NewReflectCopier[*SimpleSrc, SimpleDst]()
+				simpleSrc := &SimpleSrc{}
+				return copier.Copy(&simpleSrc)
+			},
+			wantErr: errInvalidType,
+		},
+		{
+			name: "error input int",
+			copyFunc: func() (any, error) {
+				copier := NewReflectCopier[int, SimpleDst]()
+				test := 1
+				return copier.Copy(&test)
+			},
+			wantErr: errInvalidType,
+		},
+		// Map、Slice的复杂结构应不予以支持深度拷贝
+		// 这种"二级"且内存空间较大的引用(本质还是指针)数据结构，
+		// 从内存、性能、安全上来看，需要尽可能地减少深度拷贝
+		// 所以应当把Map和Slice的管理交由调用者,从实际业务角度出发，目前不太清楚深拷贝一个Map和slice干什么，真要有，不如让具体业务自己深度拷贝
+		// 所以选择直接浅拷贝
+		// 而String类型，约等于C++中的 string_view，所以直接Set就可以，即dst中的string获得了底层字符串的一份视图
+		// http://t.csdn.cn/iMzqZ
+		{
+			name: "simple struct",
+			copyFunc: func() (any, error) {
+				copier := NewReflectCopier[SimpleInlineSrc, SimpleInlineDst]()
+				initSimpleStructParam()
+				res, err := copier.Copy(simpleInlineSrc)
+				//修改simpleInlineSrc中的string,(更换原先src中的string，即src中的string_view是 "abc"，具体见函数)
+				changeSimpleStructParam()
+				return res, err
+			},
+			wantDst: &SimpleInlineDst{
+				mapS:         map[string]int{"1": 1, "2": 2, "3": 3, "4": 4},
+				byteS:        []byte{'a', 'b', 'c', 'd'},
+				stringS:      "before",
+				stringIgnore: "",
+			},
+		},
+		{
+			name: "simple struct demo",
+			copyFunc: func() (any, error) {
+				copier := NewReflectCopier[SimpleSrc, SimpleDst]()
+				age := 18
+				simpleSrc := &SimpleSrc{
+					Name:    "大明大聪明",
+					Age:     &age,
+					Friends: []string{"Tom", "Jerry"},
+				}
+				res, err := copier.Copy(simpleSrc)
+				//都进行修改再比较，以验证深度拷贝
+				*simpleSrc.Age = 19
+				simpleSrc.Name = "大明大xx"
+				assert.Equal(t, 19, age)
+				return res, err
+			},
+			wantDst: &SimpleDst{
+				Name:    "大明大聪明",
+				Age:     ekit.ToPtr[int](18),
+				Friends: []string{"Tom", "Jerry"},
+			},
+		},
+		{
+			name: "pointer struct",
+			copyFunc: func() (any, error) {
+				copier := NewReflectCopier[SimplePointersSrc, SimplePointersDst]()
+				a := 1
+				b := float32(1.0)
+				c := float64(2.0)
+				d := uint8(1)
+				e := uint32(2)
+				f := uint64(3)
+				g := int64(-1)
+				h := int32(-2)
+				i := int8(-3)
+				j := bool(false)
+				k := complex64(complex(1, 2))
+				l := complex(3, 4)
+
+				simpleSrc := &SimplePointersSrc{
+					&a, &b, &c, &d, &e, &f, &g, &h, &i, &j, &k, &l,
+				}
+				res, err := copier.Copy(simpleSrc)
+
+				a = 2
+				b = float32(2.0)
+				c = float64(3.0)
+				d = uint8(2)
+				e = uint32(4)
+				f = uint64(5)
+				g = int64(-2)
+				h = int32(-3)
+				i = int8(-4)
+				j = bool(true)
+				k = complex64(complex(2, 3))
+				l = complex(4, 5)
+
+				return res, err
+			},
+			wantDst: &SimplePointersDst{
+				ekit.ToPtr[int](1),
+				ekit.ToPtr[float32](1.0),
+				ekit.ToPtr[float64](2.0),
+				ekit.ToPtr[uint8](1),
+				ekit.ToPtr[uint32](2),
+				ekit.ToPtr[uint64](3),
+				ekit.ToPtr[int64](-1),
+				ekit.ToPtr[int32](-2),
+				ekit.ToPtr[int8](-3),
+				ekit.ToPtr[bool](false),
+				ekit.ToPtr[complex64](complex64(complex(1, 2))),
+				ekit.ToPtr[complex128](complex(3, 4)),
+				nil,
+			},
+		},
 		// 你还需要测试
 		// 1. Src 或者 Dst 类型非法，例如基本类型，内置类型或者接口
 		// 2. 测试组合（结构体组合，指针组合，接口组合——接口组合可以直接不支持），深层组合，多重组合
@@ -61,6 +165,41 @@ func TestReflectCopier_Copy(t *testing.T) {
 	}
 }
 
+type SimpleInlineSrc struct {
+	mapS    map[string]int
+	byteS   []byte
+	stringS string
+}
+
+type SimpleInlineDst struct {
+	mapS         map[string]int
+	byteS        []byte
+	stringS      string
+	stringIgnore string
+}
+
+var simpleMap = make(map[string]int)
+var simpleSlice = make([]byte, 4)
+var simpleInlineSrc = &SimpleInlineSrc{
+	mapS:  simpleMap,
+	byteS: simpleSlice}
+
+func initSimpleStructParam() {
+	simpleMap["1"] = 1
+	simpleMap["2"] = 2
+	simpleMap["3"] = 3
+	simpleSlice[0] = 'a'
+	simpleSlice[1] = 'b'
+	simpleSlice[2] = 'c'
+	simpleInlineSrc.stringS = "before"
+}
+
+func changeSimpleStructParam() {
+	simpleMap["4"] = 4
+	simpleSlice[3] = 'd'
+	simpleInlineSrc.stringS = "after"
+}
+
 type SimpleSrc struct {
 	Name    string
 	Age     *int
@@ -71,4 +210,35 @@ type SimpleDst struct {
 	Name    string
 	Age     *int
 	Friends []string
+}
+
+type SimplePointersSrc struct {
+	a *int
+	b *float32
+	c *float64
+	d *uint8
+	e *uint32
+	f *uint64
+	g *int64
+	h *int32
+	i *int8
+	j *bool
+	k *complex64
+	l *complex128
+}
+
+type SimplePointersDst struct {
+	a *int
+	b *float32
+	c *float64
+	d *uint8
+	e *uint32
+	f *uint64
+	g *int64
+	h *int32
+	i *int8
+	j *bool
+	k *complex64
+	l *complex128
+	m *unsafe.Pointer
 }
