0a304570c6139539a7111d108103dc86f506c73a
Deng Ming <mingflycash@gmail.com>
2022-08-26 12:08:31 +0800

    ekit: 引入 golintci 和 goimports

--- Makefile
+++ Makefile
@@ -1,8 +1,29 @@
+.PHONY:	bench
 bench:
 	@go test -bench=. -benchmem  ./...
 
+.PHONY:	ut
 ut:
 	@go test -race ./...
 
+.PHONY:	setup
 setup:
-	sh ./script/setup.sh
+	@sh ./script/setup.sh
+
+.PHONY:	fmt
+fmt:
+	@sh ./script/goimports.sh
+
+.PHONY:	lint
+lint:
+	@golangci-lint run -c .golangci.yml
+
+.PHONY: tidy
+tidy:
+	@go mod tidy -v
+
+.PHONY: check
+check:
+	@$(MAKE) fmt
+	@$(MAKE) tidy
+	@$(MAKE) lint
--- script/setup.sh
+++ script/setup.sh
@@ -12,18 +12,29 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-SOURCE=.github/pre-push
-TARGET=.git/hooks/pre-push
+SOURCE_COMMIT=.github/pre-commit
+TARGET_COMMIT=.git/hooks/pre-commit
+SOURCE_PUSH=.github/pre-push
+TARGET_PUSH=.git/hooks/pre-push
 
-# copy pre-push file if not exist.
-if [ ! -f $TARGET ]; then
-    echo "设置 github hooks..."
-    cp $SOURCE $TARGET
+# copy pre-commit file if not exist.
+if [ ! -f $TARGET_COMMIT ]; then
+    echo "设置 git pre-commit hooks..."
+    cp $SOURCE_COMMIT $TARGET_COMMIT
 fi
 
-# add permission to TARGET file.
-test -x $TARGET || chmod +x $TARGET
+# copy pre-push file if not exist.
+if [ ! -f $TARGET_PUSH ]; then
+    echo "设置 git pre-push hooks..."
+    cp $SOURCE_PUSH $TARGET_PUSH
+fi
+
+# add permission to TARGET_PUSH and TARGET_COMMIT file.
+test -x $TARGET_PUSH || chmod +x $TARGET_PUSH
+test -x $TARGET_COMMIT || chmod +x $TARGET_COMMIT
 
 echo "安装 golangci-lint..."
-# install golangci-lint when golangci-lint not installed.
-go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.42.1
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
+
+echo "安装 goimports..."
+go install golang.org/x/tools/cmd/goimports@latest
