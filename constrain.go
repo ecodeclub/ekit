814b5ae4d26ad6c00091501c3a8255d05241fec2
Deng Ming <mingflycash@gmail.com>
2022-09-09 18:56:30 +0800

    slice: 聚合函数 Max, Min 和 Sum

---
+++ constrain.go
@@ -0,0 +1,27 @@
+// Copyright 2021 gotomicro
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ekit
+
+// RealNumber 实数
+// 绝大多数情况下，你都应该用这个来表达数字的含义
+type RealNumber interface {
+	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 |
+		~int | ~int8 | ~int16 | ~int32 | ~int64 |
+		~float32 | ~float64
+}
+
+type Number interface {
+	RealNumber | ~complex64 | ~complex128
+}
