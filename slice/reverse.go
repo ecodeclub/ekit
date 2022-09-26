33790227ddac50fe2108e1caf22f1dc3c77b1a64
Kevin <kaiwei.lin@gmail.com>
2022-09-26 19:43:30 +0800

    feat: 增加ReverseSelf

--- slice/reverse.go
+++ slice/reverse.go
@@ -14,10 +14,18 @@
 
 package slice
 
+// Reverse 将会完全创建一个新的切片，而不是直接在 src 上进行翻转。
 func Reverse[T comparable](src []T) []T {
 	var ret = make([]T, 0, len(src))
 	for i := len(src) - 1; i >= 0; i-- {
 		ret = append(ret, src[i])
 	}
 	return ret
+}
+
+// ReverseSelf 會直接在 src 上进行翻转。
+func ReverseSelf[T comparable](src []T) {
+	for i, j := 0, len(src)-1; i < j; i, j = i+1, j-1 {
+		src[i], src[j] = src[j], src[i]
+	}
 }
--- slice/reverse_test.go
+++ slice/reverse_test.go
@@ -16,8 +16,9 @@ package slice
 
 import (
 	"fmt"
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestReverseInt(t *testing.T) {
@@ -36,6 +37,11 @@ func TestReverseInt(t *testing.T) {
 			want: []int{},
 			name: "length of src is 0",
 		},
+		{
+			src:  nil,
+			want: []int{},
+			name: "length of src is nil",
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -45,11 +51,53 @@ func TestReverseInt(t *testing.T) {
 	}
 }
 
+func TestReverseSelfInt(t *testing.T) {
+	tests := []struct {
+		name string
+		src  []int
+		want []int
+	}{
+		{
+			want: []int{7, 5, 3, 1},
+			src:  []int{1, 3, 5, 7},
+			name: "normal test",
+		},
+		{
+			src:  []int{},
+			want: []int{},
+			name: "length of src is 0",
+		},
+		{
+			src:  nil,
+			want: []int{},
+			name: "length of src is nil",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ReverseSelf[int](tt.src)
+			assert.ElementsMatch(t, tt.want, tt.src)
+		})
+	}
+}
+
 func ExampleReverse() {
 	res := Reverse[int]([]int{1, 3, 2, 2, 4})
 	fmt.Println(res)
 	res2 := Reverse[string]([]string{"a", "b", "c", "d", "e"})
 	fmt.Println(res2)
+	// Output:
+	// [4 2 2 3 1]
+	// [e d c b a]
+}
+
+func ExampleReverseSelf() {
+	src := []int{1, 3, 2, 2, 4}
+	ReverseSelf[int](src)
+	fmt.Println(src)
+	src2 := []string{"a", "b", "c", "d", "e"}
+	ReverseSelf[string](src2)
+	fmt.Println(src2)
 	// Output:
 	// [4 2 2 3 1]
 	// [e d c b a]
