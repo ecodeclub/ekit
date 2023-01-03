b91c08f8d6f1926ae4762c36dcda61ff25446306
yzicheng <786797661@qq.com>
2023-01-03 14:49:02 +0800

    mapx: Values 和 KeysValues 方法

--- mapx/map.go
+++ mapx/map.go
@@ -23,3 +23,25 @@ func Keys[K comparable, V any](m map[K]V) []K {
 	}
 	return res
 }
+
+// Values 返回 map 里面的所有的 value。
+// 需要注意：这些 value 的顺序是随机。
+func Values[K comparable, V any](m map[K]V) []V {
+	res := make([]V, 0, len(m))
+	for k := range m {
+		res = append(res, m[k])
+	}
+	return res
+}
+
+// KeysValues 返回 map 里面的所有的 key,value。
+// 需要注意：这些 (key,value) 的顺序是随机,相对顺序是一致的。
+func KeysValues[K comparable, V any](m map[K]V) ([]K, []V) {
+	keys := make([]K, 0, len(m))
+	values := make([]V, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+		values = append(values, m[k])
+	}
+	return keys, values
+}
--- mapx/map_test.go
+++ mapx/map_test.go
@@ -60,3 +60,90 @@ func TestKeys(t *testing.T) {
 		})
 	}
 }
+func TestValues(t *testing.T) {
+	testCases := []struct {
+		name    string
+		input   map[int]int
+		wantRes []int
+	}{
+		{
+			name:    "nil",
+			input:   nil,
+			wantRes: []int{},
+		},
+		{
+			name:    "empty",
+			input:   map[int]int{},
+			wantRes: []int{},
+		},
+		{
+			name: "single",
+			input: map[int]int{
+				1: 11,
+			},
+			wantRes: []int{11},
+		},
+		{
+			name: "multiple",
+			input: map[int]int{
+				1: 11,
+				2: 12,
+			},
+			wantRes: []int{11, 12},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			res := Values[int, int](tc.input)
+			assert.ElementsMatch(t, tc.wantRes, res)
+		})
+	}
+}
+
+func TestKeysValues(t *testing.T) {
+	testCases := []struct {
+		name       string
+		input      map[int]int
+		wantKeys   []int
+		wantValues []int
+	}{
+		{
+			name:       "nil",
+			input:      nil,
+			wantKeys:   []int{},
+			wantValues: []int{},
+		},
+		{
+			name:       "empty",
+			input:      map[int]int{},
+			wantKeys:   []int{},
+			wantValues: []int{},
+		},
+		{
+			name: "single",
+			input: map[int]int{
+				1: 11,
+			},
+			wantKeys:   []int{1},
+			wantValues: []int{11},
+		},
+		{
+			name: "multiple",
+			input: map[int]int{
+				1: 11,
+				2: 12,
+			},
+			wantKeys:   []int{1, 2},
+			wantValues: []int{11, 12},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			keys, values := KeysValues[int, int](tc.input)
+			assert.ElementsMatch(t, tc.wantKeys, keys)
+			assert.ElementsMatch(t, tc.wantValues, values)
+		})
+	}
+}
