759d164ddf609ef8ab27a2b072335f8695243448
yzc <786797661@qq.com>
2023-01-09 23:49:31 +0800

    红黑树 实现内容： 完成 NewRedBlackTree,Add,Delete,Find方法 测试用例增加isRedBlackTree方法 用于判断是否为红黑树

--- internal/tree/red_black_tree.go
+++ internal/tree/red_black_tree.go
@@ -22,14 +22,14 @@ const (
 )
 
 type RedBlackTree[T any] struct {
-	root    *treeNode[T]
+	root    *TreeNode[T]
 	compare ekit.Comparator[T]
 }
 
-type treeNode[T any] struct {
+type TreeNode[T any] struct {
 	color               bool
 	key                 T
-	left, right, parent *treeNode[T]
+	left, right, parent *TreeNode[T]
 }
 
 // NewRedBlackTree 构建红黑树
@@ -41,7 +41,7 @@ func NewRedBlackTree[T any](compare ekit.Comparator[T]) *RedBlackTree[T] {
 }
 
 // Add 增加节点
-func (redBlackTree *RedBlackTree[T]) Add(node *treeNode[T]) {
+func (redBlackTree *RedBlackTree[T]) Add(node *TreeNode[T]) {
 	redBlackTree.addNode(node)
 }
 
@@ -55,18 +55,18 @@ func (redBlackTree *RedBlackTree[T]) Delete(key T) {
 }
 
 // Find 查找节点
-func (redBlackTree *RedBlackTree[T]) Find(key T) *treeNode[T] {
+func (redBlackTree *RedBlackTree[T]) Find(key T) *TreeNode[T] {
 	return redBlackTree.getTreeNode(key)
 }
 
-func (redBlackTree *RedBlackTree[T]) addNode(node *treeNode[T]) {
+func (redBlackTree *RedBlackTree[T]) addNode(node *TreeNode[T]) {
 	t := redBlackTree.root
 	if t == nil {
 		redBlackTree.root = node
 		return
 	}
 	cmp := 0
-	parent := &treeNode[T]{}
+	parent := &TreeNode[T]{}
 	for t != nil {
 		parent = t
 		cmp = redBlackTree.compare(node.key, t.key)
@@ -77,9 +77,10 @@ func (redBlackTree *RedBlackTree[T]) addNode(node *treeNode[T]) {
 		} else {
 			// 重复节点,重新覆盖还是不处理
 			t = node
+			return
 		}
 	}
-	tempNode := &treeNode[T]{
+	tempNode := &TreeNode[T]{
 		key:    node.key,
 		parent: parent,
 	}
@@ -91,12 +92,75 @@ func (redBlackTree *RedBlackTree[T]) addNode(node *treeNode[T]) {
 	redBlackTree.fixAfterAdd(tempNode)
 }
 
-func (redBlackTree *RedBlackTree[T]) deleteNode(node *treeNode[T]) {
-
-	redBlackTree.fixAfterDelete(node)
+func (redBlackTree *RedBlackTree[T]) deleteNode(node *TreeNode[T]) {
+	// node 有2个子节点,需要选一个替换原有的位置
+	if node.left != nil && node.right != nil {
+		s := redBlackTree.successor(node)
+		node = s
+	}
+	replacement := &TreeNode[T]{}
+	if node.left != nil {
+		replacement = node.left
+	} else {
+		replacement = node.right
+	}
+	if replacement != nil {
+		replacement.parent = node.parent
+	}
+	if replacement != nil {
+		replacement.parent = node.parent
+		if node.parent == nil {
+			redBlackTree.root = replacement
+		} else if node == node.parent.left {
+			node.parent.left = replacement
+		} else {
+			node.parent.right = replacement
+		}
+		node.left = nil
+		node.right = nil
+		node.parent = nil
+		if node.color {
+			redBlackTree.fixAfterDelete(node)
+		}
+	} else if node.parent == nil {
+		redBlackTree.root = nil
+	} else {
+		if node.color {
+			redBlackTree.fixAfterDelete(node)
+		}
+		if node.parent != nil {
+			if node == node.parent.left {
+				node.parent.left = nil
+			} else if node == node.parent.right {
+				node.parent.right = nil
+			}
+			node.parent = nil
+		}
+	}
 }
 
-func (redBlackTree *RedBlackTree[T]) getTreeNode(key T) *treeNode[T] {
+func (redBlackTree *RedBlackTree[T]) successor(node *TreeNode[T]) *TreeNode[T] {
+	if node == nil {
+		return nil
+	} else if node.right != nil {
+		p := node.right
+		for p.left != nil {
+			p = p.left
+		}
+		return p
+	} else {
+		p := node.parent
+		ch := node
+		for p != nil && ch != p.right {
+			ch = p
+			p = p.parent
+		}
+		return p
+	}
+
+}
+
+func (redBlackTree *RedBlackTree[T]) getTreeNode(key T) *TreeNode[T] {
 	node := redBlackTree.root
 	for node != nil {
 		cmp := redBlackTree.compare(key, node.key)
@@ -112,7 +176,7 @@ func (redBlackTree *RedBlackTree[T]) getTreeNode(key T) *treeNode[T] {
 }
 
 // fixAfterAdd 插入时着色旋转
-func (redBlackTree *RedBlackTree[T]) fixAfterAdd(x *treeNode[T]) {
+func (redBlackTree *RedBlackTree[T]) fixAfterAdd(x *TreeNode[T]) {
 	x.color = Red
 	for x != nil && x != redBlackTree.root && !x.parent.color {
 		if x.parentOf() == x.parentOf().parentOf().leftOf() {
@@ -153,12 +217,64 @@ func (redBlackTree *RedBlackTree[T]) fixAfterAdd(x *treeNode[T]) {
 }
 
 // fixAfterDelete 删除时着色旋转
-func (redBlackTree *RedBlackTree[T]) fixAfterDelete(x *treeNode[T]) {
+func (redBlackTree *RedBlackTree[T]) fixAfterDelete(x *TreeNode[T]) {
+	for x != redBlackTree.root && x.colorOf() {
+		if x == x.parent.leftOf() {
+			sib := x.parentOf().rightOf()
+			if !sib.colorOf() {
+				sib.setColor(Black)
+				sib.parentOf().setColor(Red)
+				redBlackTree.rotateLeft(x.parentOf())
+				sib = x.parentOf().rightOf()
+			}
+			if sib.leftOf().colorOf() && sib.rightOf().color {
+				sib.setColor(Red)
+				x = x.parentOf()
+			} else {
+				if sib.rightOf().colorOf() {
+					sib.leftOf().setColor(Black)
+					sib.setColor(Red)
+					redBlackTree.rotateRight(sib)
+					sib = x.parentOf().rightOf()
+				}
+				sib.setColor(x.parentOf().colorOf())
+				x.parentOf().setColor(Black)
+				sib.rightOf().setColor(Black)
+				redBlackTree.rotateLeft(x.parentOf())
+				x = redBlackTree.root
+			}
 
+		} else {
+			sib := x.parentOf().leftOf()
+			if !sib.colorOf() {
+				sib.setColor(Black)
+				x.parentOf().setColor(Red)
+				redBlackTree.rotateRight(x.parentOf())
+				sib = x.parentOf().leftOf()
+			}
+			if sib.rightOf().colorOf() && sib.leftOf().colorOf() {
+				sib.setColor(Red)
+				x = x.parentOf()
+			} else {
+				if sib.leftOf().colorOf() {
+					sib.rightOf().setColor(Black)
+					sib.setColor(Red)
+					redBlackTree.rotateLeft(sib)
+					sib = x.parentOf().leftOf()
+				}
+				sib.setColor(x.parentOf().colorOf())
+				x.parentOf().setColor(Black)
+				sib.leftOf().setColor(Black)
+				redBlackTree.rotateRight(x.parentOf())
+				x = redBlackTree.root
+			}
+		}
+	}
+	x.setColor(Black)
 }
 
 // rotateLeft 左旋转
-func (redBlackTree *RedBlackTree[T]) rotateLeft(node *treeNode[T]) {
+func (redBlackTree *RedBlackTree[T]) rotateLeft(node *TreeNode[T]) {
 	if node != nil {
 		r := node.right
 		node.right = r.left
@@ -179,7 +295,7 @@ func (redBlackTree *RedBlackTree[T]) rotateLeft(node *treeNode[T]) {
 }
 
 // rotateRight 右旋转
-func (redBlackTree *RedBlackTree[T]) rotateRight(node *treeNode[T]) {
+func (redBlackTree *RedBlackTree[T]) rotateRight(node *TreeNode[T]) {
 	if node != nil {
 		l := node.left
 		node.left = l.right
@@ -199,35 +315,35 @@ func (redBlackTree *RedBlackTree[T]) rotateRight(node *treeNode[T]) {
 	}
 }
 
-func (node *treeNode[T]) colorOf() bool {
+func (node *TreeNode[T]) colorOf() bool {
 	if node == nil {
 		return Black
 	}
 	return node.color
 }
 
-func (node *treeNode[T]) setColor(color bool) {
+func (node *TreeNode[T]) setColor(color bool) {
 	if node == nil {
 		return
 	}
 	node.color = color
 }
 
-func (node *treeNode[T]) parentOf() *treeNode[T] {
+func (node *TreeNode[T]) parentOf() *TreeNode[T] {
 	if node == nil {
 		return nil
 	}
 	return node.parent
 }
 
-func (node *treeNode[T]) leftOf() *treeNode[T] {
+func (node *TreeNode[T]) leftOf() *TreeNode[T] {
 	if node == nil {
 		return nil
 	}
 	return node.left
 }
 
-func (node *treeNode[T]) rightOf() *treeNode[T] {
+func (node *TreeNode[T]) rightOf() *TreeNode[T] {
 	if node == nil {
 		return nil
 	}
---
+++ internal/tree/red_black_tree_test.go
@@ -0,0 +1,291 @@
+package tree
+
+import (
+	"fmt"
+	"github.com/gotomicro/ekit"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNewRedBlackTree(t *testing.T) {
+	tests := []struct {
+		name    string
+		compare ekit.Comparator[int]
+		wantV   *RedBlackTree[int]
+	}{
+		{
+			name:    "int",
+			compare: compare(),
+			wantV: &RedBlackTree[int]{
+				compare: compare(),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			redBlackTree := NewRedBlackTree[int](compare())
+			assert.ElementsMatch(t, tt.wantV, redBlackTree)
+		})
+	}
+}
+func compare() ekit.Comparator[int] {
+	return ekit.ComparatorRealNumber[int]
+}
+
+// isRedBlackTree 检测是否满足红黑树
+func isRedBlackTree[T any](root *TreeNode[T]) bool {
+	// 检测节点是否黑色
+	if !root.colorOf() {
+		return false
+	}
+	// count 取最左树的黑色节点作为对照
+	count := 0
+	num := 0
+	node := root
+	for node != nil {
+		if node.color {
+			count++
+		}
+		node = node.leftOf()
+	}
+	return nodeCheck[T](root, count, num)
+}
+
+// nodeCheck 节点检测
+// 1、是否有连续的红色节点
+// 2、每条路径的黑色节点是否一致
+func nodeCheck[T any](node *TreeNode[T], count int, num int) bool {
+	if node == nil {
+		return true
+	}
+	if !node.colorOf() && !node.parent.colorOf() {
+		fmt.Println("存在连续红色节点")
+		return false
+	}
+	if node.colorOf() {
+		num++
+	}
+	if node.leftOf() == nil && node.rightOf() == nil {
+		if num != count {
+			fmt.Println("黑色节点数量不一致")
+			return false
+		}
+	}
+	return nodeCheck(node.left, count, num) && nodeCheck(node.right, count, num)
+}
+
+func TestRedBlackTree_Add(t *testing.T) {
+	isRedBlackTreecase := []struct {
+		name string
+		node *TreeNode[int]
+		want bool
+	}{
+		{
+			name: "nil",
+			node: nil,
+			want: true,
+		},
+		{
+			name: "node-nil",
+			node: nil,
+			want: true,
+		},
+		{
+			name: "root",
+			node: &TreeNode[int]{left: nil, right: nil, color: Black},
+			want: true,
+		},
+		//			 root(b)
+		//			/
+		//		   a(b)
+		{
+			name: "root-oneChild",
+			node: &TreeNode[int]{
+				left: &TreeNode[int]{
+					right: nil,
+					left:  nil,
+					color: Red,
+				},
+				right: nil,
+				color: Black,
+			},
+			want: true,
+		},
+		//			 root(b)
+		//			/	    \
+		//		   a(r)	     b(b)
+		{
+			name: "root-twoChild",
+			node: &TreeNode[int]{
+				left: &TreeNode[int]{
+					right: nil,
+					left:  nil,
+					color: Red,
+				},
+				right: &TreeNode[int]{
+					right: nil,
+					left:  nil,
+					color: Black,
+				},
+				color: Black,
+			},
+			want: false,
+		},
+		//			 root(b)
+		//			/	    \
+		//		   a(b)	     b(b)
+		//		 /  \        /    \
+		//      nil  c(r)    d(r)   nil
+		//           / \     / \
+		//          nil nil nil nil
+		{
+			name: "blackNodeNotSame",
+			node: &TreeNode[int]{
+				left: &TreeNode[int]{
+					right: &TreeNode[int]{
+						right: nil,
+						left:  nil,
+						color: Red,
+					},
+					left:  nil,
+					color: Black,
+				},
+				right: &TreeNode[int]{
+					right: nil,
+					left: &TreeNode[int]{
+						right: nil,
+						left:  nil,
+						color: Red,
+					},
+					color: Black,
+				},
+				color: Black,
+			},
+			want: true,
+		},
+		//			 root(b)
+		//			/	    \
+		//		   a(r)	     b(r)
+		//		 /  \        /    \
+		//      nil  c(r)    d(r)   nil
+		//           / \     / \
+		//          nil nil nil nil
+		{
+			name: "root-grandson",
+			node: &TreeNode[int]{
+				parent: nil,
+				left: &TreeNode[int]{
+					parent: &TreeNode[int]{
+						color: Black,
+					},
+					right: &TreeNode[int]{
+						right: nil,
+						left:  nil,
+						color: Red,
+						parent: &TreeNode[int]{
+							color: Red,
+						},
+					},
+					left:  nil,
+					color: Red,
+				},
+				right: &TreeNode[int]{
+					parent: &TreeNode[int]{
+						color: Black,
+					},
+					right: nil,
+					left: &TreeNode[int]{
+						right: nil,
+						left:  nil,
+						color: Red,
+						parent: &TreeNode[int]{
+							color: Red,
+						},
+					},
+					color: Red,
+				},
+				color: Black,
+			},
+			want: false,
+		},
+	}
+	for _, tt := range isRedBlackTreecase {
+		t.Run(tt.name, func(t *testing.T) {
+			res := isRedBlackTree[int](tt.node)
+			assert.Equal(t, tt.want, res)
+
+		})
+	}
+	tests := []struct {
+		name    string
+		k       []int
+		want    bool
+		wantErr error
+	}{
+		{
+			name: "nil",
+			k:    nil,
+			want: true,
+		},
+		{
+			name: "case1",
+			k:    []int{1, 2, 3, 4},
+			want: true,
+		},
+		{
+			name:    "same",
+			k:       []int{0, 0, 1, 2, 2, 3},
+			want:    true,
+			wantErr: nil,
+		},
+		{
+			name:    "disorder",
+			k:       []int{1, 2, 0, 3, 5, 4},
+			want:    true,
+			wantErr: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			redBlackTree := NewRedBlackTree[int](compare())
+			for i := 0; i < len(tt.k); i++ {
+				redBlackTree.Add(&TreeNode[int]{
+					key: tt.k[i],
+				})
+			}
+			res := isRedBlackTree[int](redBlackTree.root)
+			assert.Equal(t, tt.want, res)
+
+		})
+	}
+}
+
+func TestRedBlackTree_Delete(t *testing.T) {}
+
+func TestRedBlackTree_Find(t *testing.T) {}
+
+func TestRedBlackTree_addNode(t *testing.T) {}
+
+func TestRedBlackTree_deleteNode(t *testing.T) {}
+
+func TestRedBlackTree_fixAfterAdd(t *testing.T) {}
+
+func TestRedBlackTree_fixAfterDelete(t *testing.T) {}
+
+func TestRedBlackTree_getTreeNode(t *testing.T) {}
+
+func TestRedBlackTree_rotateLeft(t *testing.T) {}
+
+func TestRedBlackTree_rotateRight(t *testing.T) {}
+
+func TestRedBlackTree_successor(t *testing.T) {}
+
+func Test_treeNode_colorOf(t *testing.T) {}
+
+func Test_treeNode_leftOf(t *testing.T) {}
+
+func Test_treeNode_parentOf(t *testing.T) {}
+
+func Test_treeNode_rightOf(t *testing.T) {}
+
+func Test_treeNode_setColor(t *testing.T) {}
