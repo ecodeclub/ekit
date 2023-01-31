4c1af75c0998832ad773be28e09bba5508f8604a
Ming Deng <mingflycash@gmail.com>
2023-01-31 13:59:43 +0800

    添加 mapx 的例子，准备 v0.0.6 (#143)

--- mapx/hashmap.go
+++ mapx/hashmap.go
@@ -30,7 +30,10 @@ func (m *HashMap[T, ValType]) newNode(key Hashable, val ValType) *node[T, ValTyp
 }
 
 type Hashable interface {
+	// Code 返回该元素的哈希值
+	// 注意：哈希值应该尽可能的均匀以避免冲突
 	Code() uint64
+	// Equals 比较两个元素是否相等。如果返回 true，那么我们会认为两个键是一样的。
 	Equals(key any) bool
 }
 
