11fa6d9c3f6a897bba7af700752a73a3c6b701f0
陈明勇 <chenmingyong5873@126.com>
2023-02-07 00:49:30 +0800

    slice: FilterDelete - 新增 FilterDelete 函数，删除某些符合条件的元素。考虑到性能问题，所有操作都会在原切片上进行；被删除元素之后的元素会往前移动，有且只会一定一次 新增 FilterDelete 测试函数和编写测试用例

--- slice/delete.go
+++ slice/delete.go
@@ -21,3 +21,30 @@ func Delete[Src any](src []Src, index int) ([]Src, error) {
 	res, _, err := slice.Delete[Src](src, index)
 	return res, err
 }
+
+// FilterDelete 删除符合条件的元素
+// 考虑到性能问题，所有操作都会在原切片上进行
+// 被删除元素之后的元素会往前移动，有且只会移动一次
+func FilterDelete[Src any](src []Src, m func(idx int, src Src) bool) []Src {
+	// 记录被删除的元素位置，也称空缺的位置
+	emptyPos := -1
+	for idx := range src {
+		// 判断是否满足删除的条件
+		if m(idx, src[idx]) {
+			// 如果在此之前没有被删除的元素，记录为空缺位置，等待后续元素往前移填补
+			if emptyPos == -1 {
+				emptyPos = idx
+			}
+		} else if emptyPos != -1 { // 如果前面有空缺的位置（也就是有元素被删除了）
+			// 交换（填补）元素
+			src[emptyPos] = src[idx]
+			// 空缺位置往后移
+			emptyPos++
+		}
+	}
+	// 如果一个元素都没有删除，返回原切片
+	if emptyPos == -1 {
+		return src
+	}
+	return src[:emptyPos]
+}
--- slice/delete_test.go
+++ slice/delete_test.go
@@ -67,3 +67,120 @@ func ExampleDelete() {
 	// [1 2 4]
 	// ekit: 下标超出范围，长度 4, 下标 -1
 }
+
+func TestFilterDelete(t *testing.T) {
+
+	testCases := []struct {
+		name            string
+		src             []int
+		deleteCondition func(idx int, src int) bool
+
+		wantRes []int
+	}{
+		{
+			name: "不删除元素",
+			src:  []int{0, 1, 2, 3, 4, 5, 6, 7},
+			deleteCondition: func(idx int, src int) bool {
+				return false
+			},
+
+			wantRes: []int{0, 1, 2, 3, 4, 5, 6, 7},
+		},
+		{
+			name: "删除首位元素",
+			src:  []int{0, 1, 2, 3, 4, 5, 6, 7},
+			deleteCondition: func(idx int, src int) bool {
+				return idx == 0
+			},
+
+			wantRes: []int{1, 2, 3, 4, 5, 6, 7},
+		},
+		{
+			name: "删除前面两个元素",
+			src:  []int{0, 1, 2, 3, 4, 5, 6, 7},
+			deleteCondition: func(idx int, src int) bool {
+				return idx == 0 || idx == 1
+			},
+
+			wantRes: []int{2, 3, 4, 5, 6, 7},
+		},
+		{
+			name: "删除中间单个元素",
+			src:  []int{0, 1, 2, 3, 4, 5, 6, 7},
+			deleteCondition: func(idx int, src int) bool {
+				return idx == 3
+			},
+
+			wantRes: []int{0, 1, 2, 4, 5, 6, 7},
+		},
+		{
+			name: "删除中间多个不连续元素",
+			src:  []int{0, 1, 2, 3, 4, 5, 6, 7},
+			deleteCondition: func(idx int, src int) bool {
+				return idx == 2 || idx == 4
+			},
+
+			wantRes: []int{0, 1, 3, 5, 6, 7},
+		},
+		{
+			name: "删除中间多个连续元素",
+			src:  []int{0, 1, 2, 3, 4, 5, 6, 7},
+			deleteCondition: func(idx int, src int) bool {
+				return idx == 3 || idx == 4
+			},
+
+			wantRes: []int{0, 1, 2, 5, 6, 7},
+		},
+		{
+			name: "删除中间多个元素，第一部分为一个元素，第二部分为连续元素",
+			src:  []int{0, 1, 2, 3, 4, 5, 6, 7},
+			deleteCondition: func(idx int, src int) bool {
+				return idx == 2 || idx == 4 || idx == 5
+			},
+
+			wantRes: []int{0, 1, 3, 6, 7},
+		},
+		{
+			name: "删除中间多个元素，第一部分为连续元素，第二部分为一个元素",
+			src:  []int{0, 1, 2, 3, 4, 5, 6, 7},
+			deleteCondition: func(idx int, src int) bool {
+				return idx == 2 || idx == 3 || idx == 5
+			},
+
+			wantRes: []int{0, 1, 4, 6, 7},
+		},
+		{
+			name: "删除后面两个元素",
+			src:  []int{0, 1, 2, 3, 4, 5, 6, 7},
+			deleteCondition: func(idx int, src int) bool {
+				return idx == 6 || idx == 7
+			},
+
+			wantRes: []int{0, 1, 2, 3, 4, 5},
+		},
+		{
+			name: "删除末尾元素",
+			src:  []int{0, 1, 2, 3, 4, 5, 6, 7},
+			deleteCondition: func(idx int, src int) bool {
+				return idx == 7
+			},
+
+			wantRes: []int{0, 1, 2, 3, 4, 5, 6},
+		},
+		{
+			name: "删除所有元素",
+			src:  []int{0, 1, 2, 3, 4, 5, 6, 7},
+			deleteCondition: func(idx int, src int) bool {
+				return true
+			},
+
+			wantRes: []int{},
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			res := FilterDelete(tc.src, tc.deleteCondition)
+			assert.Equal(t, tc.wantRes, res)
+		})
+	}
+}
