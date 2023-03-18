39db8c90335acbae9e848b3bb4058b3e8025c7ad
Stone-afk <1711865140@qq.com>
2023-03-18 11:19:05 +0800

    feature taskpool 可观测性

--- pool/task_pool.go
+++ pool/task_pool.go
@@ -48,37 +48,6 @@ var (
 	defaultMaxIdleTime = 10 * time.Second
 )
 
-// TaskPool 任务池
-type TaskPool interface {
-	// Submit 执行一个任务
-	// 如果任务池提供了阻塞的功能，那么如果在 ctx 过期都没有提交成功，那么应该返回错误
-	// 调用 Start 之后能否继续提交任务，则取决于具体的实现
-	// 调用 Shutdown 或者 ShutdownNow 之后提交任务都会返回错误
-	Submit(ctx context.Context, task Task) error
-
-	// Start 开始调度任务执行。在调用 Start 之前，所有的任务都不会被调度执行。
-	// Start 之后，能否继续调用 Submit 提交任务，取决于具体的实现
-	Start() error
-
-	// Shutdown 关闭任务池。如果此时尚未调用 Start 方法，那么将会立刻返回。
-	// 任务池将会停止接收新的任务，但是会继续执行剩下的任务，
-	// 在所有任务执行完毕之后，用户可以从返回的 chan 中得到通知
-	// 任务池在发出通知之后会关闭 chan struct{}
-	Shutdown() (<-chan struct{}, error)
-
-	// ShutdownNow 立刻关闭线程池
-	// 任务池能否中断当前正在执行的任务，取决于 TaskPool 的具体实现，以及 Task 的具体实现
-	// 该方法会返回所有剩下的任务，剩下的任务是否包含正在执行的任务，也取决于具体的实现
-	ShutdownNow() ([]Task, error)
-}
-
-// Task 代表一个任务
-type Task interface {
-	// Run 执行任务
-	// 如果 ctx 设置了超时时间，那么实现者需要自己决定是否进行超时控制
-	Run(ctx context.Context) error
-}
-
 // TaskFunc 一个可执行的任务
 type TaskFunc func(ctx context.Context) error
 
@@ -558,4 +527,15 @@ func (b *OnDemandBlockTaskPool) numOfGo() int32 {
 	n = b.totalGo
 	b.mutex.RUnlock()
 	return n
+}
+
+func (b *OnDemandBlockTaskPool) Stats() (chan State, error) {
+	ch := make(chan State, 1)
+	ch <- State{
+		PoolState:      b.state,
+		GoCnt:          b.totalGo,
+		QueueSize:      cap(b.queue),
+		WaitingTaskCnt: len(b.queue),
+	}
+	return ch, nil
 }
