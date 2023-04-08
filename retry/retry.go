0203b6e191b4e999f3e961c3487f43df851d43dc
Mingyong Chen <67659676+chenmingyong0423@users.noreply.github.com>
2023-04-08 12:30:42 +0800

    retry: 指数退避重试策略实现 (#174)
    ---------
    
    Co-authored-by: 陈明勇 <chenmingyong5873@126.com>
    Co-authored-by: root <root@LAPTOP-AA809J7R.localdomain>

--- retry/retry.go
+++ retry/retry.go
@@ -15,6 +15,7 @@
 package retry
 
 import (
+	"math"
 	"sync/atomic"
 	"time"
 
@@ -41,6 +42,49 @@ func (s *FixedIntervalRetryStrategy) Next() (time.Duration, bool) {
 	retries := atomic.AddInt32(&s.retries, 1)
 	if s.maxRetries <= 0 || retries <= s.maxRetries {
 		return s.interval, true
+	}
+	return 0, false
+}
+
+type ExponentialBackoffRetryStrategy struct {
+	// 初始重试间隔
+	initialInterval time.Duration
+	// 最大重试间隔
+	maxInterval time.Duration
+	// 最大重试次数
+	maxRetries int32
+	// 当前重试次数
+	retries int32
+	// 是否已经达到最大重试间隔
+	maxIntervalReached atomic.Value
+}
+
+func NewExponentialBackoffRetryStrategy(initialInterval, maxInterval time.Duration, maxRetries int32) (*ExponentialBackoffRetryStrategy, error) {
+	if initialInterval <= 0 {
+		return nil, errs.NewErrInvalidIntervalValue(initialInterval)
+	} else if initialInterval > maxInterval {
+		return nil, errs.NewErrInvalidMaxIntervalValue(maxInterval, initialInterval)
+	}
+	return &ExponentialBackoffRetryStrategy{
+		initialInterval: initialInterval,
+		maxInterval:     maxInterval,
+		maxRetries:      maxRetries,
+	}, nil
+}
+
+func (s *ExponentialBackoffRetryStrategy) Next() (time.Duration, bool) {
+	retries := atomic.AddInt32(&s.retries, 1)
+	if s.maxRetries <= 0 || retries <= s.maxRetries {
+		if reached, ok := s.maxIntervalReached.Load().(bool); ok && reached {
+			return s.maxInterval, true
+		}
+		interval := s.initialInterval * time.Duration(math.Pow(2, float64(retries-1)))
+		// 溢出或当前重试间隔大于最大重试间隔
+		if interval <= 0 || interval > s.maxInterval {
+			s.maxIntervalReached.Store(true)
+			return s.maxInterval, true
+		}
+		return interval, true
 	}
 	return 0, false
 }
