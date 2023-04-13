9e87c613804f2e4a54a40b0e2f031414c9fc2dc6
Deng Ming <mingflycash@gmail.com>
2023-04-13 14:09:43 +0800

    准备 v0.0.7 发版

---
+++ retry/fixed_internal.go
@@ -0,0 +1,47 @@
+// Copyright 2021 ecodeclub
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package retry
+
+import (
+	"sync/atomic"
+	"time"
+
+	"github.com/ecodeclub/ekit/internal/errs"
+)
+
+// FixedIntervalRetryStrategy 等间隔重试
+type FixedIntervalRetryStrategy struct {
+	maxRetries int32         // 最大重试次数，如果是 0 或负数，表示无限重试
+	interval   time.Duration // 重试间隔时间
+	retries    int32         // 当前重试次数
+}
+
+func NewFixedIntervalRetryStrategy(interval time.Duration, maxRetries int32) (*FixedIntervalRetryStrategy, error) {
+	if interval <= 0 {
+		return nil, errs.NewErrInvalidIntervalValue(interval)
+	}
+	return &FixedIntervalRetryStrategy{
+		maxRetries: maxRetries,
+		interval:   interval,
+	}, nil
+}
+
+func (s *FixedIntervalRetryStrategy) Next() (time.Duration, bool) {
+	retries := atomic.AddInt32(&s.retries, 1)
+	if s.maxRetries <= 0 || retries <= s.maxRetries {
+		return s.interval, true
+	}
+	return 0, false
+}
