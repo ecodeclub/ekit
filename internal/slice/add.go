843af75706a84d253ec4423ad686edee4288bfd6
JhonWong <1290773033@qq.com>
2023-07-31 16:09:55 +0800

    添加泛型Add函数,在指定位置插入元素

---
+++ internal/slice/add.go
@@ -0,0 +1,35 @@
+// Copyright 2021 ecodeclub
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package slice
+
+import "github.com/ecodeclub/ekit/internal/errs"
+
+func Add[T any](src []T, element T, index int) ([]T, error) {
+	length := len(src)
+	if index < 0 || index >= length {
+		return nil, errs.NewErrIndexOutOfRange(length, index)
+	}
+
+	//先将src扩展一个元素
+	var zeroValue T
+	src = append(src, zeroValue)
+	for i := len(src) - 1; i > index; i-- {
+		if i-1 >= 0 {
+			src[i] = src[i-1]
+		}
+	}
+	src[index] = element
+	return src, nil
+}
