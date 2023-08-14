4a09f173134282ded2e485dfb86fac175313f3a8
Ming Deng <mingflycash@gmail.com>
2023-08-14 16:26:35 +0800

    slice: 重构 slice 中使用 equalFunc 的方法 (#205)
    1. 引入 matchFunc
    2. 简化 slice 中查找类型的 API
    3. 引入 Find 和 FindAll 方法

--- slice/index.go
+++ slice/index.go
@@ -17,17 +17,17 @@ package slice
 // Index 返回和 dst 相等的第一个元素下标
 // -1 表示没找到
 func Index[T comparable](src []T, dst T) int {
-	return IndexFunc[T](src, dst, func(src, dst T) bool {
+	return IndexFunc[T](src, func(src T) bool {
 		return src == dst
 	})
 }
 
-// IndexFunc 返回和 dst 相等的第一个元素下标
+// IndexFunc 返回 match 返回 true 的第一个下标
 // -1 表示没找到
 // 你应该优先使用 Index
-func IndexFunc[T any](src []T, dst T, equal equalFunc[T]) int {
+func IndexFunc[T any](src []T, match matchFunc[T]) int {
 	for k, v := range src {
-		if equal(v, dst) {
+		if match(v) {
 			return k
 		}
 	}
@@ -37,7 +37,7 @@ func IndexFunc[T any](src []T, dst T, equal equalFunc[T]) int {
 // LastIndex 返回和 dst 相等的最后一个元素下标
 // -1 表示没找到
 func LastIndex[T comparable](src []T, dst T) int {
-	return LastIndexFunc[T](src, dst, func(src, dst T) bool {
+	return LastIndexFunc[T](src, func(src T) bool {
 		return src == dst
 	})
 }
@@ -45,9 +45,9 @@ func LastIndex[T comparable](src []T, dst T) int {
 // LastIndexFunc 返回和 dst 相等的最后一个元素下标
 // -1 表示没找到
 // 你应该优先使用 LastIndex
-func LastIndexFunc[T any](src []T, dst T, equal equalFunc[T]) int {
+func LastIndexFunc[T any](src []T, match matchFunc[T]) int {
 	for i := len(src) - 1; i >= 0; i-- {
-		if equal(dst, src[i]) {
+		if match(src[i]) {
 			return i
 		}
 	}
@@ -56,17 +56,17 @@ func LastIndexFunc[T any](src []T, dst T, equal equalFunc[T]) int {
 
 // IndexAll 返回和 dst 相等的所有元素的下标
 func IndexAll[T comparable](src []T, dst T) []int {
-	return IndexAllFunc[T](src, dst, func(src, dst T) bool {
+	return IndexAllFunc[T](src, func(src T) bool {
 		return src == dst
 	})
 }
 
-// IndexAllFunc 返回和 dst 相等的所有元素的下标
+// IndexAllFunc 返回和 match 返回 true 的所有元素的下标
 // 你应该优先使用 IndexAll
-func IndexAllFunc[T any](src []T, dst T, equal equalFunc[T]) []int {
+func IndexAllFunc[T any](src []T, match matchFunc[T]) []int {
 	var indexes = make([]int, 0, len(src))
 	for k, v := range src {
-		if equal(v, dst) {
+		if match(v) {
 			indexes = append(indexes, k)
 		}
 	}
--- slice/map.go
+++ slice/map.go
@@ -49,7 +49,9 @@ func toMap[T comparable](src []T) map[T]struct{} {
 func deduplicateFunc[T any](data []T, equal equalFunc[T]) []T {
 	var newData = make([]T, 0, len(data))
 	for k, v := range data {
-		if !ContainsFunc[T](data[k+1:], v, equal) {
+		if !ContainsFunc[T](data[k+1:], func(src T) bool {
+			return equal(src, v)
+		}) {
 			newData = append(newData, v)
 		}
 	}
