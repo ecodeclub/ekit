791101b604ea4ef56b80060f1d6e318a9cea0f23
wureny <wurenyu19@gmail.com>
2023-10-09 13:31:26 +0800

    修改了测试用例

--- sqlx/newnull.go
+++ sqlx/newnull.go
@@ -42,29 +42,5 @@ func NewNullTime(val time.Time) sql.NullTime {
 }
 
 func NewNullBytes(val []byte) sql.NullString {
-	return sql.NullString{String: string(val), Valid: val != nil}
-}
-
-func NewNullStringPtr(val string) *sql.NullString {
-	return &sql.NullString{String: val, Valid: val != ""}
-}
-
-func NewNullInt64Ptr(val int64) *sql.NullInt64 {
-	return &sql.NullInt64{Int64: val, Valid: val != 0}
-}
-
-func NewNullFloat64Ptr(val float64) *sql.NullFloat64 {
-	return &sql.NullFloat64{Float64: val, Valid: val != 0}
-}
-
-func NewNullBoolPtr(val bool) *sql.NullBool {
-	return &sql.NullBool{Bool: val, Valid: val}
-}
-
-func NewNullTimePtr(val time.Time) *sql.NullTime {
-	return &sql.NullTime{Time: val, Valid: !val.IsZero()}
-}
-
-func NewNullBytesPtr(val []byte) *sql.NullString {
-	return &sql.NullString{String: string(val), Valid: val != nil}
+	return sql.NullString{String: string(val), Valid: len(val) > 0}
 }
