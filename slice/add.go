194ef90fbedfc183592146cb34b156e8f877050d
Dxyinme <32793868+dxyinme@users.noreply.github.com>
2024-01-22 19:32:10 +0800

    slice.Reverse, slice.Add, arrayList.Add 修改 (#242)

--- slice/add.go
+++ slice/add.go
@@ -17,7 +17,8 @@ package slice
 import "github.com/ecodeclub/ekit/internal/slice"
 
 // Add 在index处添加元素
-// index 范围应为[0, len(src))
+// index 范围应为[0, len(src)]
+// 如果index == len(src) 则表示往末尾添加元素
 func Add[Src any](src []Src, element Src, index int) ([]Src, error) {
 	res, err := slice.Add[Src](src, element, index)
 	return res, err
