317856e841367bcd17b06674abf68dcb3a0bad68
david.wong <bigdavidwong@gmail.com>
2024-11-07 16:37:53 +0800

    IfThenElseFunc固定增加错误返回，泛型T作为用户可以自定义返回的结果

--- condition.go
+++ condition.go
@@ -24,7 +24,7 @@ func IfThenElse[T any](condition bool, trueValue, falseValue T) T {
 }
 
 // IfThenElseFunc 根据条件执行对应的函数并返回泛型结果
-func IfThenElseFunc[T any](condition bool, trueFunc, falseFunc func() T) T {
+func IfThenElseFunc[T any](condition bool, trueFunc, falseFunc func() (T, error)) (T, error) {
 	if condition {
 		return trueFunc()
 	}
--- condition_test.go
+++ condition_test.go
@@ -37,26 +37,29 @@ func ExampleIfThenElse() {
 }
 
 func TestIfThenElseFunc(t *testing.T) {
-	err := IfThenElseFunc(true, func() error {
-		return nil
-	}, func() error {
-		return errors.New("some error")
+	resp, err := IfThenElseFunc(true, func() (int, error) {
+		return 0, nil
+	}, func() (int, error) {
+		return 1, errors.New("some error")
 	})
 	assert.NoError(t, err)
+	assert.Equal(t, resp, 0)
 }
 
 func ExampleIfThenElseFunc() {
-	err := IfThenElseFunc(false, func() error {
+	code, err := IfThenElseFunc(false, func() (code int, err error) {
 		// do something when condition is true
 		// ...
-		return nil
-	}, func() error {
+		return 0, nil
+	}, func() (code int, err error) {
 		// do something when condition is false
 		// ...
-		return errors.New("some error when execute func2")
+		return 1, errors.New("some error when execute func2")
 	})
+	fmt.Println(code)
 	fmt.Println(err)
 
 	// Output:
+	// 1
 	// some error when execute func2
 }
