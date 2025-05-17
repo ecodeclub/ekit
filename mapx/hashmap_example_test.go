b04f4875d2a0a5b0f7094045e096bf7524813c60
dxyinme <dxyinme@outlook.com>
2025-05-17 12:45:00 +0800

    fix license and goci-lint

--- mapx/hashmap_example_test.go
+++ mapx/hashmap_example_test.go
@@ -1,3 +1,17 @@
+// Copyright 2021 ecodeclub
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package mapx
 
 import (
@@ -25,9 +39,9 @@ func (ts testStringData) Equals(other any) bool {
 func ExampleHashMap_Iterate() {
 	hashMap := NewHashMap[testStringData, int](0)
 	strArr := make([]string, 0)
-	hashMap.Put(testStringData{data: "hello"}, 1)
-	hashMap.Put(testStringData{data: "world"}, 2)
-	hashMap.Put(testStringData{data: "ekit"}, 3)
+	_ = hashMap.Put(testStringData{data: "hello"}, 1)
+	_ = hashMap.Put(testStringData{data: "world"}, 2)
+	_ = hashMap.Put(testStringData{data: "ekit"}, 3)
 
 	hashMap.Iterate(
 		func(key testStringData, val int) bool {
--- mapx/multi_map_example_test.go
+++ mapx/multi_map_example_test.go
@@ -1,3 +1,17 @@
+// Copyright 2021 ecodeclub
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package mapx
 
 import (
@@ -10,11 +24,11 @@ import (
 func ExampleMultiMap_Iterate() {
 	multiMap := NewMultiHashMap[testStringData, int](0)
 	arr := make([]pair.Pair[string, int], 0)
-	multiMap.Put(testStringData{data: "hello"}, 1)
-	multiMap.Put(testStringData{data: "world"}, 2)
-	multiMap.Put(testStringData{data: "world"}, 3)
-	multiMap.Put(testStringData{data: "world"}, 4)
-	multiMap.Put(testStringData{data: "ekit"}, 3)
+	_ = multiMap.Put(testStringData{data: "hello"}, 1)
+	_ = multiMap.Put(testStringData{data: "world"}, 2)
+	_ = multiMap.Put(testStringData{data: "world"}, 3)
+	_ = multiMap.Put(testStringData{data: "world"}, 4)
+	_ = multiMap.Put(testStringData{data: "ekit"}, 3)
 
 	multiMap.Iterate(
 		func(key testStringData, val int) bool {
